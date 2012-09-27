9588d248734a177e3477a927f75e54149fc6791f
Hua Xin <guohuaxin@gmail.com>
2012-09-27 22:03:22 +0800

    initial commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function(){
+//console.log("test HX's first js");
+
+    $("img").on("dblclick", function(){
+        $("img").width(200);
+
+        $("h1").fadeOut();
+
+        $(".booboo").slideUp();
+
+        $("#rahul").hide();
+
+    });
+
+//    $("img").click(function(){
+//
+//    $("img").width(200);
+//    });
+});
